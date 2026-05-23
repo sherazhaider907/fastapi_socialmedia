8c8a7b4cf6a02d25e4df605f231f0d8d04149126
SherazHaider907 <Sherazhaider665@gmail.com>
2026-05-23 13:07:50 +0500

    delete opration

--- main.py
+++ main.py
@@ -22,7 +22,11 @@ def find_post(id):
     for p in my_posts:
         if p['id'] == id:
             return p
-        
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 
 @app.get("/")
@@ -34,7 +38,7 @@ async def get_posts():
     return {"data":my_posts}
 
 
-@app.post("/posts")
+@app.post("/posts",status_code=status.HTTP_201_CREATED)
 async def create_post(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 1000000)
@@ -47,3 +51,13 @@ async def get_post(id: int):
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
     return {"post_detail": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(id: int):
+    index = find_index_post(id)
+
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
