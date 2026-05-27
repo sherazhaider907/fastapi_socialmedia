120cdb11250c4c1d9d409829ee809599e03ef38a
SherazHaider907 <Sherazhaider665@gmail.com>
2026-05-27 18:06:08 +0500

    get user by id

--- app/main.py
+++ app/main.py
@@ -114,3 +114,17 @@ async def create_user(user: schemas.UserCreate, db: db_dependency):
     db.refresh(new_user)
 
     return new_user
+
+# get users
+@app.get("/users/{id}", response_model=schemas.UserOut)
+async def get_user(id: int, db: db_dependency):
+
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"user with id: {id} was not found"
+        )
+
+    return user
