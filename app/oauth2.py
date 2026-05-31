fd1c6a35ffd1c8063d17f3dd5565e1a53e0dcbec
SherazHaider907 <Sherazhaider665@gmail.com>
2026-05-31 20:57:17 +0500

    post fixes

--- app/oauth2.py
+++ app/oauth2.py
@@ -3,7 +3,8 @@ from datetime import datetime, timedelta
 from .schemas import TokenData
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-
+from .dependencies import db_dependency
+from . import models
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
@@ -40,6 +41,10 @@ def verify_access_token(token: str, credentials_exception):
     
     return token_data
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(db: db_dependency,token: str = Depends(oauth2_scheme) ,):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
-    return verify_access_token(token, credentials_exception)
+    token_data = verify_access_token(token, credentials_exception)
+    user = db.query(models.User).filter(models.User.id == token_data.id).first()
+    if not user:
+        raise credentials_exception
+    return user
--- app/routers/post.py
+++ app/routers/post.py
@@ -17,14 +17,14 @@ router = APIRouter(
 
 # Get all posts
 @router.get("/", response_model=List[schemas.Post])
-async def get_posts(db: db_dependency,user_id: int = Depends(oauth2.get_current_user)):
+async def get_posts(db: db_dependency, current_user: int = Depends(oauth2.get_current_user)):
     posts = db.query(models.Post).all()
     return posts
 
 
 # Create post
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-async def create_post(post: schemas.PostCreate, db: db_dependency, user_id: int = Depends(oauth2.get_current_user)):
+async def create_post(post: schemas.PostCreate, db: db_dependency, current_user: int = Depends(oauth2.get_current_user)):
 
     new_post = models.Post(**post.dict())
     db.add(new_post)
@@ -36,7 +36,7 @@ async def create_post(post: schemas.PostCreate, db: db_dependency, user_id: int 
 
 # Get single post
 @router.get("/{id}", response_model=schemas.Post)
-async def get_post(id: int, db: db_dependency,user_id: int = Depends(oauth2.get_current_user)):
+async def get_post(id: int, db: db_dependency,current_user: int = Depends(oauth2.get_current_user)):
 
     post = db.query(models.Post).filter(models.Post.id == id).first()
 
@@ -51,7 +51,7 @@ async def get_post(id: int, db: db_dependency,user_id: int = Depends(oauth2.get_
 
 # Delete post
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(id: int, db: db_dependency, user_id: int = Depends(oauth2.get_current_user)):
+async def delete_post(id: int, db: db_dependency, current_user: int = Depends(oauth2.get_current_user)):
 
     post_query = db.query(models.Post).filter(models.Post.id == id)
 
@@ -71,7 +71,7 @@ async def delete_post(id: int, db: db_dependency, user_id: int = Depends(oauth2.
 
 # Update post
 @router.put("/{id}", response_model=schemas.Post)
-async def update_post(id: int, updated_post: schemas.PostCreate, db: db_dependency, user_id: int = Depends(oauth2.get_current_user)):
+async def update_post(id: int, updated_post: schemas.PostCreate, db: db_dependency, current_user: int = Depends(oauth2.get_current_user)):
 
     post_query = db.query(models.Post).filter(models.Post.id == id)
 
