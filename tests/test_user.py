94da4e682f71ba96c81531790410b0327c6120be
SherazHaider907 <Sherazhaider665@gmail.com>
2026-06-07 22:20:22 +0500

    pytest add fixture for new user

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,6 +1,22 @@
 import uuid
 from app import schemas
 from .database import client , session
+import pytest
+from app.config import settings
+from jose import jwt
+
+@pytest.fixture
+def test_user(client):
+    user_data = {
+        "email": f"{uuid.uuid4()}@example.com",
+        "password": "password123"
+    }
+    response = client.post("/users/", json=user_data)
+    assert response.status_code == 201
+    new_user = response.json()          
+    new_user["password"] = user_data["password"]   
+    return new_user
+
 
 
 def test_root(client):
@@ -21,4 +37,20 @@ def test_create_user(client):
     new_user = schemas.UserOut(**response.json())
     assert new_user.email == random_email
     assert response.status_code == 201
-    
+
+
+def test_login_user(client, test_user):
+    response = client.post(
+        "/login",
+        data={
+            "username": test_user["email"],  
+            "password": test_user["password"] 
+        }
+    )
+
+    login_response = schemas.Token(**response.json())
+    payload = jwt.decode(login_response.access_token, settings.secret_key, settings.algorithm)
+    id = payload.get("user_id")
+    assert id == test_user["id"]
+    assert login_response.token_type == "bearer"
+    assert response.status_code == 200
